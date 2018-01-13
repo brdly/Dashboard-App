11feb823cd4967512aa8cdca629f62118e7fa4d6
Rob Marsh <robs@ourcoder.com>
2018-01-13 19:56:09 +0000

    Updated UI, Started Pulling JSON Data

--- public/adminportal.php
+++ public/adminportal.php
@@ -9,27 +9,25 @@
     <title>Admin Portal - Group 4</title>
     <meta name="description" content="Admin Portal for SPAT Task - Group 4.">
 
-    <link href="/css/spat/build/js/slider/demo/style.css" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=BenchNine:700" rel="stylesheet">
+    <link href="/SPATProject/public/build/js/slider/demo/style.css" rel="stylesheet">
     <script src="http://code.jquery.com/jquery-1.10.2.min.js"></script>
     <script src="http://code.jquery.com/ui/1.10.3/jquery-ui.min.js"></script>
-    <script src="/css/spat/build/js/slider/jQDateRangeSlider-min.js"></script>
+    <script src="/SPATProject/public/build/js/slider/jQDateRangeSlider-min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.1/Chart.min.js"></script>
 
     <style>
 
-        @font-face {
-            font-family: Seguibl;
-            src: url(/css/spat/build/css/seguibl.ttf);
-        }
         
         body {
+            font-weight:100;
             height:100vh;
             width:100vw;
             margin:0;
             padding:0;
             background-color:white;
             color:white;
-            font-family: 'Seguibl', sans-serif;
+            font-family: 'BenchNine', sans-serif;
             font-size:16px;
             text-align:center;
         }
@@ -89,17 +87,17 @@
         header > section {
             float:right;
             background-color:rgb(45,62,80);
-            height:40px;
-            min-width:100px;
-            width:auto;
-            padding-left:10px;
-            padding-right:10px;
-            margin-right:20px;
-            cursor:pointer;
-            margin-top:15px;
-            line-height:40px;
-            border-radius:3px;
-            font-size: 18px;
+            height: 50px;
+            min-width: 100px;
+            width: auto;
+            padding-left: 30px;
+            padding-right: 10px;
+            margin-right: 20px;
+            cursor: pointer;
+            margin-top: 10px;
+            line-height: 50px;
+            border-radius: 3px;
+            font-size: 28px;
         }
 
         header > section:hover {
@@ -116,18 +114,20 @@
         nav > section {
             float:left;
             background-color:rgb(45,62,80);
-            height:42px;
+            height:36px;
             width:158px;
             margin-left:6px;
-            margin-bottom:12px;
-            line-height:42px;
+            margin-bottom:7px;
+            line-height:36px;
             cursor:pointer;
             border-radius:3px;
-            font-size: 16px;
+            font-size: 22px;
+            border-bottom: 1px solid rgba(24,187,156,0.3);
         }
 
         nav > section:hover {
             background-color:rgba(24,187,156,0.5);
+            border-bottom: 1px solid transparent;
         }
 
         .menuOn {
@@ -158,9 +158,8 @@
 
         #slider {
             float:left;
-            width:calc(100vw - 260px);
-            margin-left:20px;
-            margin-top:10px;
+            width:calc(100vw - 320px);
+            margin-left:50px;
             margin-bottom:10px;
             height:40px;
         }
@@ -179,13 +178,22 @@
         }
 
         .titlebox {
-            margin-left:25px;
-            font-size:22px;
-            line-height: 24px;
+            padding-left: 10px;
+            margin-left: 25px;
+            background-image: url(build/img/ilo.png);
+            background-repeat: no-repeat;
+            background-position: center center;
+            background-size: contain;
+            font-size: 20px;
+            margin-top: 8px;
+            color: rgb(24,187,156);
+            line-height: 112px;
+            color:rgb(24,187,156);
+        }
         }
 
-        .hide { display:none;}
-        .show { display:block;}
+        .hide { display:none!important;}
+        .show { display:block!important;}
 
         footer {
 
@@ -211,29 +219,177 @@
             height:auto!important;
         }
 
+        .min{
+            border-bottom: 1px solid transparent!important;
+            height:20px;
+        }
+
+        #Upwork {
+            background-image: url(build/img/upwork.png);
+            background-repeat: no-repeat;
+            background-size: 35px auto;
+            background-position: 5px center;
+        }
+
+        #Fiverr {
+            background-image: url(build/img/fiverr.png);
+            background-repeat: no-repeat;
+            background-size: 35px auto;
+            background-position: 10px center;
+            padding-right:0px;
+        }
+
+        #AmazonMechanicalTurk {
+            background-image: url(build/img/amt.png);
+            background-repeat: no-repeat;
+            background-size: 45px auto;
+            background-position: 10px 5px;
+            padding-right:5px;
+        }
+
     </style>
 
     <script>
 
     $( document ).ready(function() {
 
-        $("#slider").dateRangeSlider();
+        var allData;
+        var selectedData = []; //for chosen platforms, auto-update the data on a timer
+        var selectedOpts = [];
+        var oldestDate;
+        var newestDate;
+        var option;
+        var platform;
+        var menuAction = "disable";
 
-        $( ".button" ).click(function() {
-            $( this ).toggleClass( "menuOn" );
-        });     
+        $("#slider").bind("valuesChanged", function(e, data){
+            console.log("Values just changed. min: " + data.values.min + " max: " + data.values.max);
+        });
 
         $( ".chart" ).click(function() {
             var orig = this;
             var check = $( orig ).hasClass( "chartBig" );
+
             if(check === false) {
+
                 $('.chart').each(function(index, obj){
                     if(orig.id !== obj.id) {$("#"+obj.id).insertAfter("#"+orig.id);}
                     $( "#"+obj.id ).removeClass( "chartBig" );
                 });
+
                 $( orig ).toggleClass( "chartBig" );
             }
         });
+
+        $.getJSON('data.json', function(data) {  
+
+            allData = data;
+
+            $( "header" ).append( '<section id="'+data.app_name.replace(/ /g, '')+'" class="floatLeft hoverOff titlebox">'+data.app_name+'</section>' );
+            
+            $.each(data.platforms, function (index, value) {
+                if(value === "Amazon Mechanical Turk") {
+                    $( "header" ).append( '<section id="'+value.replace(/ /g, '')+'" title="'+value+'" class="button">AMT</section>' );
+                } else {
+                    $( "header" ).append( '<section id="'+value.replace(/ /g, '')+'" title="'+value+'" class="button">'+value+'</section>' );
+                }
+            });
+
+            $.each(data.form_fields, function (index, value) {
+                $( "nav" ).append( '<section id="'+value.replace(/ /g, '')+'" title="'+value+'" class="button2">'+value+'</section>' );
+            });
+            //$.each(data.platforms, function (index, name) {
+            //    var meep = data[name];
+            //})
+
+            $( ".button" ).click(function() {
+                $( this ).toggleClass( "menuOn" );
+
+                if($( this ).hasClass( "menuOn" )) {
+                    menuAction = "enable";
+                } else { menuAction = "disable"; };
+
+                platform = this.title;
+                var data = allData[platform];
+
+                if(menuAction == "enable") { 
+                    var newArr = [platform];
+                    newArr.push(data);
+                    selectedData.push(newArr);
+                 } else {
+                    $.each(selectedData, function (index, value) {
+                        if(value[0] == platform) { 
+                            selectedData.splice( index, 1 );
+                        }
+                    })
+                    oldestDate = null;
+                    newestDate = null;
+                }
+
+                var count = selectedData.length;
+
+                $.each(selectedData, function (index, platform_data) {
+                    $.each(platform_data, function (index, value) {
+                        var dt = new Date(value.Date);  
+                        if(!oldestDate || oldestDate > dt) oldestDate = dt;
+                        if(!newestDate || newestDate < dt) newestDate = dt;
+                    })
+                })
+
+                $("#slider").dateRangeSlider({
+                    bounds:{
+                        min: oldestDate,
+                        max: newestDate
+                    },
+                    defaultValues:{
+                            min:  oldestDate,
+                            max: newestDate
+                        },
+                        valueLabels:"change",
+                        delayOut: 4000
+                    });
+                    loadTimeBar();
+                });
+
+                function loadTimeBar() {
+                    if(menuAction == "disable") { 
+                        //need to check if all platforms disabled - hide time bar
+                        var count = 0;
+                        $('.button').each(function(index, obj){
+                            if($( obj ).hasClass( "menuOn" )) count++;;
+                        });
+                        if(count === 0) { 
+                            $( "#slider" ).addClass( "hide" ); 
+                            $("#slider").dateRangeSlider("destroy");
+                        }
+                    } else {
+                        $("#slider").dateRangeSlider(); 
+                        $( "#slider" ).removeClass( "hide" );
+                    }
+                }
+
+                $( ".button2" ).click(function() { 
+                    $( this ).toggleClass( "menuOn" );
+                    option = this.title;
+                    if(selectedData) {
+                        $.each(selectedData, function (index, platform_data) {
+                            var newArr1 = [];
+                            newArr1.push(platform_data[0]);
+                            $.each(platform_data[1], function (index, value) {
+                                var newArr2 = [option];
+                                newArr2.push(value[option]);
+                                newArr1.push(newArr2);
+                            })
+                                selectedOpts.push(newArr1);
+                        })
+                    } else { 
+                        //please choose a platform 
+                    }
+                    debugger;
+                 }); 
+        });
+
+        
 
     })
     </script>
@@ -246,6 +402,7 @@
 
 <header>
 
+<!--
     <section class="floatLeft hoverOff titlebox">ADMIN<br/>PORTAL</section>
     <section id="at" class="menuOn button">Amazon Turk</section>
     <section id="cf" class="button">CrowdFlower</section>
@@ -253,14 +410,16 @@
     <section id="cc" class="button">CloudCrowd</section>
     <section id="fv" class="button">Fiverr</section>
     <section id="up" class="button">Upwork</section>
+//-->
 
 </header>
 
 <div id="navigationLeftSpacer"></div>
 
 <nav>
-
-    <section class="button hoverOff"> </section>
+<section class="min hoverOff"> </section>
+<!--
+    <section class="hoverOff"> </section>
     <section class="button menuOn">Overall Rating</section>
     <section class="button">Work/Life Balance</section>
     <section class="button">Worker Benefits</section>
@@ -269,6 +428,7 @@
     <section class="button">Countries</section>
     <section class="button">Reviews</section>
     <section class="button">LOGOUT</section>
+    //-->
 
 </nav>
 
@@ -277,11 +437,11 @@
 
 <main>
 
-    <div id="slider"></div>
+    <div id="slider" class="hide"></div>
 
-    <section id="chartOne" class="chart chartBig">  <?php include_once("barMagic.php"); ?> </section>
-    <section id="chartTwo" class="chart"> <?php include_once("lineMagic.php");  ?> </section>
-    <section id="chartThree" class="chart"> <?php include_once("doughnutMagic.php");  ?> </section>
+    <section id="chartOne" class="chart chartBig ">  <?php include_once("barMagic.php"); ?> </section>
+    <section id="chartTwo" class="chart hide"> <?php include_once("lineMagic.php");  ?> </section>
+    <section id="chartThree" class="chart hide"> <?php include_once("doughnutMagic.php");  ?> </section>
 
 </main>
 
