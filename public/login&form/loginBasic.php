8a56a39c6cdc6f00e07b8be5de3659025801d34b
ReclusiveChimp <nicolescu.mircea@yahoo.com>
2018-01-16 15:01:26 +0000

    updated login form
    collab with Mikus, login connects to db properly

--- public/login&form/loginBasic.php
+++ public/login&form/loginBasic.php
@@ -1,283 +1,111 @@
 <?php
-    if (session_status() == PHP_SESSION_NONE) {
-        session_start();
-    }
+if (session_status() == PHP_SESSION_NONE) {
+    session_start();
+}
 ?>
+
+
+
 <!DOCTYPE html>
 <html>
 <head>
-
-    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
-    <base href="">
-    <meta charset="utf-8">
-    <title>Login Page - Group 4</title>
-    <meta name="description" content="Login Page for SPAT Task - Group 4.">
-
-    <link href="/css/spat/build/js/slider/demo/style.css" rel="stylesheet">
-    <script src="http://code.jquery.com/jquery-1.10.2.min.js"></script>
-    <script src="http://code.jquery.com/ui/1.10.3/jquery-ui.min.js"></script>
-    <script src="/css/spat/build/js/slider/jQDateRangeSlider-min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.1/Chart.min.js"></script>
-
+    <title>Form</title>
     <style>
 
-        @font-face {
-            font-family: Seguibl;
-            src: url(/css/spat/build/css/seguibl.ttf);
-        }
-
-        body {
-            height:100vh;
-            width:100vw;
-            margin:0;
-            padding:0;
-            background-color:white;
-            color:black;
-            font-family: 'Seguibl', sans-serif;
-            font-size:16px;
-            text-align:center;
-        }
-
-        #headerTopSpacer {
-            float:left;
-            background-color:white;
-            width:100vw;
-            height:0px;
-        }
-
-        #headerLeftSpacer {
-            float:left;
-            background-color:rgb(24,187,156);
-            width:10px;
-            height:70px;
-        }
-
-        #navigationLeftSpacer {
-            float:left;
-            clear: both;
-            background-color:rgb(24,187,156);
-            width:10px;
-            height:calc(100vh - 70px);
-        }
-
-        #mainLeftSpacer {
-            float: left;
-            background-color:rgb(45,62,80);
-            width:10px;
-            height:calc(100vh - 70px);
-            overflow:hidden;
-        }
-
-        #mainLeftSpacer2 {
-            float: left;
-            background-color: white;
-            width: 6px;
-            height: 100vh;
-            border-radius: 5px;
-            border-left: 27px white solid;
-        }
-
-        #mainTopSpacer {
-            float: left;
-            background-color: rgb(45,62,80);
-            width: calc(100vw - 190px);
-        }
-
-        header {
-            float:left;
-            background-color:rgb(45,62,80);
-            width: calc(100vw - 10px);
-            height: 70px;
-        }
-
-        header > section {
-            float:right;
-            background-color:rgb(45,62,80);
-            height:40px;
-            min-width:100px;
-            width:auto;
-            padding-left:10px;
-            padding-right:10px;
-            margin-right:20px;
-            cursor:pointer;
-            margin-top:15px;
-            line-height:40px;
-            border-radius:3px;
-            font-size: 18px;
-        }
-
-        header > section:hover {
-            background-color:rgba(24,187,156,0.5);
-        }
-
-        nav {
-            float:left;
-            background-color:rgb(45,62,80);
-            height:calc(100vh - 70px);
-            width:170px;
-        }
-
-        nav > section {
-            float:left;
-            background-color:rgb(45,62,80);
-            height:42px;
-            width:158px;
-            margin-left:6px;
-            margin-bottom:12px;
-            line-height:42px;
-            cursor:pointer;
-            border-radius:3px;
-            font-size: 16px;
-        }
-
-        nav > section:hover {
-            background-color:rgba(24,187,156,0.5);
-        }
-
-
-
-        main {
-            float:left;
-            background-color:white;
-            width:calc(100vw - 190px);
-            height:calc(100vh - 71px);
-            overflow:auto;
-        }
-
-        main > section {
-            float:left;
-            background-color:white;
-            width:360px;
-            height:200px;
-            margin:10px;
-            line-height:250px;
-        }
-
-        footer {
-            float:left;
-            background-color:white;
-        }
-
-
-        .floatLeft {
-            float:left!important;
-            color: white;
-        }
-
-        .hoverOff:hover {
-            background-color:transparent!important;
-            cursor:auto!important;
-        }
-
-        .titlebox {
-            margin-left:25px;
-            font-size:22px;
-            line-height: 24px;
-        }
-
-
-
-
-
-
-        form
+        body
         {
+            background-color: beige;
+            color: beige;
+
+        }
+
+        #login
+        {
+            width: 35%;
+            background-color:rgb(45,62,80);
             text-align: center;
-            padding: 10%;
+            margin: auto;
+            margin-top: 5%;
+            padding: 3%;
+            border-radius: 3%;
+
+        }
+
+        #login  > .loginBling > p
+        {
+            margin: auto;
+            margin-bottom: 20%;
+            width: 70%;
+        }
+
+        #login *  > p
+        {
+            margin: auto;
+            margin-top: 10%;
+            padding: 5%;
 
 
         }
 
-        label
+        input
         {
-            display: block;
+            display: inline-block;
+            width: 36%;
+            background-color: transparent;
+            color: grey;
             padding: 2%;
         }
 
-
-
-        #loginForm
+        #submit
         {
-
-
-        }
-
-
-
-        #loginInfo
-        {
-            background-color: rgba(24,187,156,0.5);
-            margin-top: -5%;
-            margin-left: 20%;
-            padding: 5%;
-            border-radius:5px;
-        }
-
-        #infoRequest
-        {
-            font-size: 28px;
-            margin-top: 1%;
-        }
-
-        button
-        {
-            display: block;
+            width: 20%;
             margin: auto;
-            margin-top: 5%;
+            margin-top: 20%;
+            display: block;
+
+
         }
+
+        .loginBling
+        {
+            background-color:rgb(24,187,156);
+        }
+
+        #loginForm_failed
+        {
+            font-size: 30px;
+            color:red;
+        }
+
 
 
 
     </style>
-
-
-
 </head>
 <body>
+<div id="login">
 
-<div id="headerTopSpacer"></div>
-<div id="headerLeftSpacer"></div>
+    <?php
+    if (isset($_SESSION['login'])){
+        echo "<p id='loginForm_failed'>".$_SESSION['login']."</p>";
+        $_SESSION['login'] = null;
+    }
+    ?>
 
-<header>
-
-    <section class="floatLeft hoverOff titlebox">LOGIN<br/>PAGE</section>
-
-</header>
-
-
-<div id="navigationLeftSpacer"></div>
-
-
-<nav></nav>
-
-<div id="mainLeftSpacer"><div id="mainLeftSpacer2"></div></div>
-<div id="mainTopSpacer"></div>
-
-<div id="loginForm">
+    <section class="loginBling"> <p><strong>Please enter your email and password:</strong></p></section>
     <form name="form1" method="post" action="../../public/adminportal.php">
-        <div id="loginInfo">
-            <p id="infoRequest"><strong>Please enter your username and password:</strong></p>
-            <?php
-            if (isset($_SESSION['login'])){
-                echo "<p id='loginForm_failed'>".$_SESSION['login']."</p>";
-                $_SESSION['login'] = null;
-            }
-            ?>
-            <label><b>Username</b></label>
-            <input type="text" placeholder="Enter Username" name="Username" required>
+        <div>
+            <input type="text" placeholder="Email Address" name="Username" required>
 
-            <label><b>Password</b></label>
-            <input type="password" placeholder="Enter Password" name="Password" required>
+            <input type="password" placeholder="Password" name="Password" required>
 
-            <button type="submit">Login</button>
-
-            <p>Don't have an account yet? Click here to sign up</p>
+            <button id="submit" type="submit">Login</button>
+            <section class="loginBling"><p>
+                <b>If you do not have an account, click <a href="">here</a> to sign up!</b>
+                </p></section>
         </div>
     </form>
 </div>
-
-<footer></footer>
-
 </body>
+
 </html>
